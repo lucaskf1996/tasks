a78dc6586faf6197f78752be260bd927e8e73a89
lucaskf1996 <email@example.com>
2021-11-19 23:52:20 -0300

    teste

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
