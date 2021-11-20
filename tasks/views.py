fc4aa8f3cc2752f2d4603f1675f9ba1283443312
lucaskf1996 <email@example.com>
2021-11-20 00:12:41 -0300

    teste

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Task
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    all_tasks = Task.objects.all()
+    return HttpResponse(all_tasks)
